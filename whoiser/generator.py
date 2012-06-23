0cdb4d6d70feca08e1439f5ae5c18e1c6f0af7ae
Kristaps Kulis <kristaps.kulis@gmail.com>
2012-06-23 06:10:58 +0300

    automatic TLD whois generator

---
+++ whoiser/generator.py
@@ -0,0 +1,55 @@
+import os 
+from tld import get_whois_list
+from string import Template
+WHOIS_INTERFACE = """ #TODO: write implementation
+from servers.generic import WhoisQuery
+class WhoisQuery(WhoisQuery):
+    server = '${server}'
+    def parse_response(self, response):
+        return response
+"""
+NO_WHOIS_INTERFACE = """
+from servers.generic import WhoisQuery
+class WhoisQuery(WhoisQuery):
+    def query(self, query):
+        raise NotImplementedError(u"TLD $tld has no Whois server available")
+"""
+WHOIS_TEMPLATE = Template(WHOIS_INTERFACE)
+NO_WHOIS_TEMPLATE = Template(NO_WHOIS_INTERFACE)
+MY_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+def get_implementation(tld, server):
+    if server:
+        return WHOIS_TEMPLATE.substitute(tld = tld, server = server)
+    else:
+        return NO_WHOIS_TEMPLATE.substitute(tld = tld)
+def write_implementation(tld, code):
+    filename = os.path.join(MY_ROOT, 'servers')
+    filename = os.path.join(filename, '%s.py' % tld)
+    print filename 
+    f = open(filename, "w+")
+    f.write(code)
+    f.close() 
+def write_tld_list(tld_list):
+    imports = []
+    code = [u"TLD_LIST = ["]
+    for tld in tld_list:
+        code.append('("%s", %s.WhoisQuery)' % (tld,tld))
+        imports.append(u"from servers import %s" % tld)
+    code = u"\n".join(code)
+    imports = u"\n".join(imports)
+    filename = os.path.join(MY_ROOT, 'servers')
+    filename = os.path.join(filename, "__init__.py")
+    print filename
+    f = open(filename, "w+")
+    f.write(imports)
+    f.write(code)
+    f.close()
+def update_implementations():
+    tld_list = []
+    for tld, server in get_whois_list():
+        implementation = get_implementation(tld, server)
+        write_implementation(tld, implementation)
+        tld_list.append(tld)
+    write_tld_list(tld_list)
+update_implementations()
