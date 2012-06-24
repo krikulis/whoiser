d968844ad76aebf965a5450cf2dd42796321a719
Kristaps Kulis <kristaps.kulis@gmail.com>
2012-06-24 23:50:22 +0300

    .aero functionality

--- whoiser/servers/AERO.py
+++ whoiser/servers/AERO.py
@@ -1,6 +1,7 @@
- #TODO: write implementation
+from utils import string_to_dict
 from servers.generic import GenericWhoisQuery
 class WhoisQuery(GenericWhoisQuery):
     server = 'whois.aero'
+    unavailable_key = "NOT FOUND\n"
     def parse_response(self, response):
         return response
